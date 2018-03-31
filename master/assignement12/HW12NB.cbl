c0a7eca737b3e298443dc80da4207e7528af3104
NickedWhileShaving <best.nikolina1@yahoo.com>
2018-03-31 17:03:47 -0500

    Fixed some more.

--- master/assignement12/HW12NB.cbl
+++ master/assignement12/HW12NB.cbl
@@ -14,22 +14,26 @@
            05 NUM-IN               PIC X(2).
                88 IS-VALID             VALUE "01" THRU "99".
            05 NUM-IN-INT REDEFINES
-              NUM-IN               PIC 99.
+              NUM-IN                   PIC 99.
+           05 NUM-IN-DECR              PIC X(2).
+              88 IS-VALID-DECR             VALUE "01" THRU "99".
+           05 NUM-IN-INT-DECR REDEFINES
+              NUM-IN-DECR             PIC S99.
+           05 NUM-IN-TEXT             PIC X(2).
+
            
        PROCEDURE DIVISION.
            DISPLAY "PLEASE ENTER A STARTING NUMBER OF BOTTLES"
-           PERFORM 1110-INPUT-CHECK.
-           PERFORM VARYING BOTTLES FROM NUM-IN-INT BY -1 UNTIL
-               BOTTLES = -1
+           PERFORM 1000-INPUT-CHECK.
+           DISPLAY "PLEASE ENTER THE DECREMENTER"
+           PERFORM 2000-INPUT-CHECK.
+           MOVE NUM-IN-DECR TO NUM-IN-TEXT.
+           MULTIPLY -1 BY NUM-IN-INT-DECR.
+           PERFORM VARYING BOTTLES FROM NUM-IN-INT BY NUM-IN-INT-DECR UNTIL
+               BOTTLES < NUM-IN-INT-DECR
                DISPLAY SPACES
                SUBTRACT 1 FROM BOTTLES GIVING REMAINING-BOTTLES
                EVALUATE BOTTLES
-                   WHEN 0
-                       DISPLAY "NO MORE BOTTLES OF BEER ON THE WALL, "
-                               "NO MORE BOTTLES OF BEER..."
-                       DISPLAY "GET THE HAT AND PASS IT AROUND, "
-                               "TIME TO B DOUBLE E DOUBLE R U-N, "
-                               "BEER RUN."
                    WHEN 1
                        DISPLAY "1 BOTTLE OF BEER ON THE WALL, "
                                "1 BOTTLE OF BEER."
@@ -51,18 +55,30 @@
                          COUNTING FOR LEADING ZEROS
                        ADD 1 TO COUNTING GIVING STARTING-POSITION
                        SUBTRACT COUNTING FROM 2 GIVING POSITIONS
-                       DISPLAY "TAKE ONE DOWN, PASS IT AROUND, "
+                       DISPLAY "TAKE " NUM-IN-TEXT " ONE DOWN, PASS IT "
+                               "AROUND, "
                                REMAINING-BOTTLES
                                (STARTING-POSITION:POSITIONS)
                                " BOTTLES OF BEER ON THE WALL."
                END-EVALUATE
            END-PERFORM
+           DISPLAY "NO MORE BOTTLES OF BEER ON THE WALL, "
+                   "NO MORE BOTTLES OF BEER..."
+           DISPLAY "GET THE HAT AND PASS IT AROUND, "
+                   "TIME TO B DOUBLE E DOUBLE R U-N, "
+                   "BEER RUN."
            ACCEPT POSITIONS.
            STOP RUN.
 
-       1110-INPUT-CHECK.
+       1000-INPUT-CHECK.
            PERFORM until IS-VALID
              DISPLAY "ENTER INTEGER IN RANGE 00-99"
              ACCEPT NUM-IN
+           END-PERFORM.
+
+       2000-INPUT-CHECK.
+           PERFORM until IS-VALID-DECR
+             DISPLAY "ENTER INTEGER IN RANGE 00-99"
+             ACCEPT NUM-IN-DECR
            END-PERFORM.
 
