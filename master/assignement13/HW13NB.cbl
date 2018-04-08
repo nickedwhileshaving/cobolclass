494fd7c08357e41d69580e88d5299ea31facbb2a
NickedWhileShaving <best.nikolina1@yahoo.com>
2018-04-08 11:15:58 -0500

    Periodic checkin of 13.

--- master/assignement13/HW13NB.cbl
+++ master/assignement13/HW13NB.cbl
@@ -1,5 +1,8 @@
-        IDENTIFICATION DIVISION.
-       PROGRAM-ID. COMMCALC.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HW13NB.
+       AUTHOR. NIKOLINA BEST.
+       DATE-WRITTEN. 04/08/18.
+
       * ASK USER FOR THE NUMBER OF UNITS SOLD AND THE UNIT PRICE.
       * DISPLAY THE PRICE AND UNITS ENTERED. 
       * CALCULATE THE GROSS SALES AMOUNT (UNITS X PRICE).  
@@ -11,77 +14,109 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
         
-       01  WS-TODAYS-DATE          pic x(08).
-       01  WS-TIME                 PIC 9(08).
-       01  WS-END                  pic X(01).
+       01  WS-TODAYS-DATE             PIC X(08).
+       01  WS-TIME                    PIC 9(08).
+       01  WS-END                     PIC X(01).
        
-       01  WS-UNITS-IN             PIC .....
-       01  WS-UNITS-NUMERIC        PIC .....
+       01  WS-UNITS-IN                PIC X(03).
+       01  WS-UNITS-NUMERIC           PIC 9(03).
        
        01  WS-UNIT-RATE-IN.
-           05 WS-UNIT-RATE-DOLLARS  .........
-           05 WS-UNIT-RATE-DECIMAL  .........
-           05 WS-UNIT-RATE-CENTS    .........
+           05 WS-UNIT-RATE-DOLLARS    PIC X(3).
+           05 WS-UNIT-RATE-DECIMAL    PIC X.
+           05 WS-UNIT-RATE-CENTS      PIC X(2).
+       01  WS-NUMERIC-RATE            PIC 999V99.
        
        01  WS-NUMERIC-UNIT-PARTS.
-           05 WS-NUMERIC-UNIT-DOLLARS 
-                                    .........
-           05 WS-NUMERIC-UNIT-CENTS .........
-       01  WS-NUMERIC-UNIT-WHOLE    .........           
+           05 WS-NUMERIC-UNIT-DOLLARS PIC X(3).
+           05 WS-NUMERIC-UNIT-CENTS   PIC X(2).
+       01  WS-NUMERIC-UNIT-WHOLE      PIC 999V99.         
        
-       01  WS-COMM-RATE            PIC ....     VALUE .09.
+       01  WS-COMM-RATE               PIC V99 VALUE .09.
 
-       01  WS-GROSS-AMOUNT         PIC .......  VALUE 0.
-       01  WS-COMM-AMOUNT          PIC .......  VALUE 0.
-       01  WS-NET-PROFIT           PIC 9(06)V99 VALUE 0.
-       
-       01  WS-FORMATTED-AMOUNT-Z   PIC ..............
-       01  WS-FORMATTED-AMOUNT     PIC ..............       
-       01  WS-FORMATTED-UNITS      PIC ....
-       
+       01  WS-GROSS-SALES             PIC 9(4)V99 VALUE 0.
+       01  WS-COMM-AMOUNT             PIC 999V99 VALUE 0.
+       01  WS-NET-PROFIT              PIC 9(6)V99 VALUE 0.
+
+       01  WS-UNITS-SOLD-FORM         PIC ZZZZZZZZZZZZZ9.
+       01  WS-RATE-ENTERED-FORM       PIC $ZZZZZZZZZZ.99.     
+       01  WS-GROSS-SALES-FORM        PIC ZZZZZZZZZZZ.99.
+       01  WS-COMMISSION-FORM         PIC ZZZZZZZZZZZ.99.
+       01  WS-PROFIT-AMOUNT-FORM      PIC $ZZZZZZZZZZ.99.
+
+       01  WS-DASHED-LINE             PIC X(30)
+                                 VALUE '______________________________'.
+       01  WS-DATE-TIME-LINE.
+           05 WS-DATE-TIME-BEGIN      PIC X(10) 
+                                 VALUE ' ON DATE: '.
+           05 WS-DATE-LINE-DATE       PIC X(8).
+           05 WS-DATE-TIME-MIDDLE     PIC X(5)
+                                 VALUE ' AT: '.
+           05 WS-DATE-LINE-TIME       PIC X(8).
+
        PROCEDURE DIVISION.
        0000-MAINLINE.
-           
-           DISPLAY 'START COMMCAFL BY YOUR NAME'.
-           perform 1000-get-date-time.
+           DISPLAY 'START HW13NB BY NIKI BEST.'.
+           PERFORM 1000-GET-DATE-TIME.
+           PERFORM 1100-GET-USER-RATE.
+           PERFORM 1200-GET-USER-UNITS.
+           PERFORM 1300-CONVERT-TO-NUMERICS.
+           PERFORM 2000-DISPLAY-VALUES.
         
+       1000-GET-DATE-TIME.
+             MOVE FUNCTION CURRENT-DATE (1:8) TO WS-DATE-LINE-DATE.                                
+             MOVE FUNCTION CURRENT-DATE (9:8) TO WS-DATE-LINE-TIME.
+
+       1100-GET-USER-RATE.
       * PROMPT THE USER FOR RATE UNTIL NUMERIC RATE ENTERED: 
-           PERFORM UNTIL ..................
-                         ..................
+           PERFORM UNTIL WS-UNIT-RATE-DOLLARS IS NUMERIC
+                     AND WS-UNIT-RATE-CENTS IS NUMERIC
                      AND WS-UNIT-RATE-DECIMAL = '.'
              DISPLAY "ENTER THE UNIT PRICE FROM 000.00 TO 999.99"
                      " AND PRESS ENTER"
              ACCEPT WS-UNIT-RATE-IN
-           END-PERFORM
-           
+           END-PERFORM.
+       
+       1200-GET-USER-UNITS.
       * PROMPT THE USER FOR UNITS UNTIL NUMERIC UNITS ENTERED:          
-           PERFORM UNTIL ........... IS NUMERIC
+           PERFORM UNTIL WS-UNITS-IN IS NUMERIC
              DISPLAY "ENTER THE UNITS SOLD FROM 000 TO 999"
                      " AND PRESS ENTER"
              ACCEPT WS-UNITS-IN
-           END-PERFORM           
+           END-PERFORM.
+
+       1300-CONVERT-TO-NUMERICS.
       * CONVERT THE STRING DATA ENTERED INTO NUMERIC FIELD:
-           .....
-           .....
-           .....
-           .....
-           .....
-           display "______________________________".
-           
+           MOVE WS-UNITS-IN TO WS-UNITS-NUMERIC.
+           MOVE WS-UNIT-RATE-IN TO WS-NUMERIC-RATE.
+
+       2000-DISPLAY-VALUES.
+           DISPLAY WS-DASHED-LINE.
       * FORMAT UNITS IN AND DISPLAY:
-           .....
-           .....       
-           
+           MOVE WS-UNITS-NUMERIC TO WS-UNITS-SOLD-FORM.
+           DISPLAY 'UNITS SOLD   : ' WS-UNITS-SOLD-FORM.
+
       * FORMAT AND DISPLAY UNIT PRICE:            
-           MOVE .............
-           DISPLAY "RATE ENTERED : " WS-FORMATTED-AMOUNT.
+           MOVE WS-NUMERIC-RATE TO WS-RATE-ENTERED-FORM.
+           DISPLAY 'RATE ENTERED : ' WS-RATE-ENTERED-FORM.
         
       * COMPUTE, FORMAT AND DISPLAY GROSS SALES:             
-           COMPUTE WS-GROSS-AMOUNT = ...............
-           
-           MOVE WS-GROSS-AMOUNT   TO ................
-           DISPLAY "GROSS SALES  : " ................  
+           COMPUTE WS-GROSS-SALES = WS-NUMERIC-RATE
+                           * WS-UNITS-NUMERIC.
+           MOVE WS-GROSS-SALES   TO WS-GROSS-SALES-FORM.
+           DISPLAY 'GROSS SALES  : ' WS-GROSS-SALES-FORM.
            
       * COMPUTE, FORMAT AND DISPLAY COMMISSION:             
-           COMPUTE WS-COMM-AMOUNT = .................
+           COMPUTE WS-COMM-AMOUNT = WS-GROSS-SALES 
+                           * WS-COMM-RATE.
+           MOVE WS-COMM-AMOUNT TO WS-COMMISSION-FORM.
+           DISPLAY 'COMMISSION   : ' WS-COMMISSION-FORM.
+
+           COMPUTE WS-NET-PROFIT = WS-GROSS-SALES
+                           - WS-COMM-AMOUNT.
+           MOVE WS-NET-PROFIT TO WS-PROFIT-AMOUNT-FORM
+           DISPLAY 'PROFIT AMOUNT: ' WS-PROFIT-AMOUNT-FORM.
+           DISPLAY WS-DASHED-LINE.
+           DISPLAY 'PROGRAM ENDS'.
+           DISPLAY WS-DATE-TIME-LINE.
                                         .
